3fbbe658dadefb819a1610bfa5a552b54d72fbdf
MachariaMark <official1offmark@gmail.com>
2019-09-23 20:47:14 +0300

    added the appropriate import relative path, submit quote constructor

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from 'src/app/quotes';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  newQuote = new Quote(0,'','','',new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+  }
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {  
   }
-
 }
