fe5f30d2fc9687b9d6eccf513254dc1750fe70e3
MachariaMark <official1offmark@gmail.com>
2019-09-23 06:44:59 +0300

    added a constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,6 @@
 export class Quotes {
+    showDescription: boolean;
+    constructor(public id: number,public name: string,public author: string,public description: string){
+      this.showDescription=false;
+    }
 }
