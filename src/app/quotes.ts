afa8c69394b53b5200651bcd58e4f2e3997c263d
MachariaMark <official1offmark@gmail.com>
2019-09-23 20:27:31 +0300

    added an updated verion of the constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
-export class Quotes {
+export class Quote {
     showDescription: boolean;
-    constructor(public id: number,public name: string,public author: string,public description: string){
+    constructor(public id: number,public name: string,public author: string,public description: string,public mediumDate: Date){
       this.showDescription=false;
     }
 }
