faddf5593c1fa9d1b313a501f662e547bf7c5165
MachariaMark <official1offmark@gmail.com>
2019-09-23 06:47:11 +0300

    added updated context

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  header = 'The Quote for today is';
-  Quotes = '"IN THIS TEMPLE AS IN THE HEARTS OF THE PEOPLE FOR WHOM HE SAVED THE UNION THE MEMORY OF ABRAHAM LINCOLN IS ENSHRINED FOREVER."';
-  Author = 'Royal Cortissoz'
+  
 }
