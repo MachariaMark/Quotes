10a51cdce42876a561e9da22e0373399c4ec5889
MachariaMark <official1offmark@gmail.com>
2019-09-24 00:21:43 +0300

    restructured constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,13 @@
 export class Quote {
-    showDescription: boolean;
-    constructor(public id: number,public name: string,public author: string,public description: string,public mediumDate: Date){
-      this.showDescription=false;
-    }
+  showDescription: boolean;
+  constructor( 
+    public id: number,
+    public quote: string, 
+    public author: string, 
+    public submitter: string, 
+    public description: string,
+    public mediumDate: Date,) 
+    {
+    this.showDescription = false;
+  }
 }
